f05017a3e63e50abb3b1011fddfc163c61e89c80
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-02 23:54:24 +0800

    PATH

--- build.sh
+++ build.sh
@@ -15,7 +15,9 @@ pwd
 export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
 curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
 gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
+mkdir -p ~/bin/
 curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+export PATH=~/bin/:$PATH
 
 repo version
 
