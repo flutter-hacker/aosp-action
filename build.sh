69559b08484dbf6c8c587e707ab42ba724873550
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-02 23:48:47 +0800

    repo

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -v # show current command
+set -x
+
+function checkResult() {
+    if [ $? -ne 0 ]; then
+        echo "Last command failed, exit."
+        exit $?
+    fi
+}
+
+pwd
+
+export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
+curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
+gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
+curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+
+repo version
+
+df -h
+
+echo "Done!"
